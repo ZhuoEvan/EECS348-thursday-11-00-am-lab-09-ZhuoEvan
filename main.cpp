057be6483bdf081563d8e7a44e2a56e4bf691b70
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2025-04-08 13:02:27 -0500

    Added Functions | 4/8/25
    Started work on functions inside the Matrix Class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 //main.cpp
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector> 
+#include <iostream> //Standard Input Output Stream
+#include <fstream> //Standard File Stream
+#include <string> //Standard String
+#include <vector> //Standard Vector
 
 using namespace std; //Reduce the width of the code | removes std::
 
@@ -13,22 +13,49 @@ class Matrix {
 
         vector<vector<T>> data;
         size_t theSize;
+        
+    public:    
 
-    public:
+        //Matrix: Constructor
+        Matrix(vector<vector<T>> matrix, size_t n) {
+            data = matrix;
+            theSize = n;
+        };
 
-        Matrix(const int n) {
+        //Matrix: Destructor
+        ~Matrix() {}
 
-        }
-
+        //Matrix: Print Function
         void print() {
             for (size_t i = 0; i < theSize; ++i) {
                 for (size_t j = 0; j < theSize; ++j) {
-                    cout << Matrix::matrix[i][j] << " ";
+                    if (data[i][j] < 10) {
+                        cout << " " << data[i][j] << " ";
+                    } else {
+                        cout << data[i][j] << " ";
+                    }
                 }
                 cout << endl; //Move to the next row
             }
             cout << endl; //Adds an extra line at the bottom of the finished matrix
         }
+
+        //Matrix: Add Function
+        void add(const Matrix<T> other) {
+            vector<vector<T>> resultMatrix;
+
+            for (size_t i = 0; i < theSize; ++i) {
+                for (size_t j = 0; j < theSize; ++j) {
+                    resultMatrix[i][j] = data[i][j] + other.data[i][j];
+                }
+            }
+            Matrix<T> result(resultMatrix, size_t theSize);
+            cout << "Result of Matrix Addition" << endl;
+            result.print();
+        }
+
+
+
 };
 
 //Out of Bounds Function
@@ -42,26 +69,109 @@ bool inBounds(const int i, const int n) {
 }
 
 //Size Check Function
-bool sizeCheck (const int n) {
-    return (n > 0);
+bool sizeCheck (const size_t n) {
+    if (n > 0) return true; //Valid Size
+    else { //Display Error Message
+        cout << "Error 02: Negative/Decimal Size" << endl;
+        return false; //Invalid Size
+    }
 }
 
 //Type Flag Check Function
 bool typeFlagCheck (const int typeFlag) {
-    return (typeFlag >= 0 && typeFlag < 2);
+    if (typeFlag >= 0 && typeFlag < 2) return true; //Valid typeFlag
+    else { //Display Error Message
+        cout << "Error 03: Invalid typeFlag" << endl;
+        return false; //Invalid typeFlag
+    };
 }
 
-//Extract Function
-void extract(string fileName) {
-    int num;
+//Valid Header Check Function
+bool validHeader(const size_t n, const int t) {
+    return (sizeCheck(n) && typeFlagCheck(t)); //Check Size and typeFlag
+}
+
+//Matrix Result Function for Integer
+void matrixResult(Matrix<int> mx1, Matrix<int> mx2) {
+    cout << "Matrix 1" << endl;
+    mx1.print();
+    cout << "Matrix 2" << endl;
+    mx2.print();
+
+    mx1.add(mx2);
+}
+
+//Matrix Result Function for Double
+void matrixResult(Matrix<double> mx1, Matrix<double> mx2) {
+    return;
+}
+
+//Build Matrix Function
+void buildMatrix(string fileName) {
     //Local Variables
+    size_t n;
+    int t;
+
+    //
     ifstream inputFile(fileName);
-    while (getline(num, inputFile)) {
-        cout << num << endl;
-    }
+    inputFile >> n >> t;
+    if (validHeader(n, t) && t == 0) {
+        cout << "Integer Matrix" << endl;
+        //Initialize the Matrices
+        vector<vector<int>> mx1;
+        vector<vector<int>> mx2;
 
+        //Resizing the Matrices to size n
+        mx1.resize(n, vector<int>(n));
+        mx2.resize(n, vector<int>(n));
 
+        //
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                inputFile >> mx1[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                inputFile >> mx2[i][j];
+            }
+        }
+        inputFile.close(); //Close the File
 
+        //
+        Matrix<int> matrix1(mx1, n);
+        Matrix<int> matrix2(mx2, n);
+        matrixResult(matrix1, matrix2);
+
+    } else if (validHeader(n, t) && t == 1) {
+        cout << "Double Matrix" << endl;
+        //Initialize the Matrices
+        vector<vector<double>> mx1;
+        vector<vector<double>> mx2;
+
+        //Resizing the Matrices to size n
+        mx1.resize(n, vector<double>(n));
+        mx2.resize(n, vector<double>(n));
+
+        //
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                inputFile >> mx1[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                inputFile >> mx2[i][j];
+            }
+        }
+        inputFile.close(); //Close the File
+
+        //
+        Matrix<double> matrix1(mx1, n);
+        Matrix<double> matrix2(mx2, n);
+        matrixResult(matrix1, matrix2);
+
+    } return;
 }
 
 //Main Function
@@ -69,13 +179,11 @@ int main() {
     cout << "[Starting Lab 9 main.cpp Program]" << endl; //Start Program Message
     //Local Variables
     string fileName;
-    int n, typeFlag;
 
     //REMEMBER TO REMOVE THE COMMENT LINE!
     //cout << "Enter the file name with dot extension: ", cin >> fileName;
     fileName = "matrix.txt";
-    
-    extract(fileName);
+    buildMatrix(fileName);
 
     cout << "[Lab 9 main.cpp Program is Completed]" << endl;
     return 0; //Program Run was Successful
