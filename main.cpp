cb982fd9cf5f93564e94e525445b88ac27145999
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2025-04-09 11:34:37 -0500

    added more functions 4/9/25 update

--- main.cpp
+++ main.cpp
@@ -40,23 +40,88 @@ class Matrix {
             cout << endl; //Adds an extra line at the bottom of the finished matrix
         }
 
+        //Matrix: Print Function (Vector)
+        void printVector(const vector<vector<T>> vectorData, const size_t n) {
+            for (size_t i = 0; i < n; ++i) {
+                for (size_t j = 0; j < n; ++j) {
+                    if (vectorData[i][j] < 10) {
+                        cout << " " << vectorData[i][j] << " ";
+                    } else {
+                        cout << vectorData[i][j] << " ";
+                    }
+                }
+                cout << endl; //Move to the next row
+            }
+            cout << endl; //Adds an extra line at the bottom of the finished matrix
+
+        }
+
         //Matrix: Add Function
         void add(const Matrix<T> other) {
-            vector<vector<T>> resultMatrix;
+            vector<vector<T>> resultMatrix(theSize, vector<T>(theSize));
 
             for (size_t i = 0; i < theSize; ++i) {
                 for (size_t j = 0; j < theSize; ++j) {
                     resultMatrix[i][j] = data[i][j] + other.data[i][j];
                 }
             }
-            Matrix<T> result(resultMatrix, size_t theSize);
-            cout << "Result of Matrix Addition" << endl;
-            result.print();
+            cout << "Add Matrix Display" << endl;
+            printVector(resultMatrix, theSize);
+        }
+
+        //Matrix: Multiply Function
+        void multiply(const Matrix<T> other) {
+            vector<vector<T>> resultMatrix(theSize, vector<T>(theSize));
+
+            for (size_t i = 0; i < theSize; ++i) {
+                for (size_t j = 0; j < theSize; ++j) {
+                    double sum = 0;
+                    for (size_t k = 0; k < theSize; ++k) {
+                        double product = data[i][k] * other.data[k][j];
+                        sum += product;
+                    }
+                    resultMatrix[i][j] = sum;
+                }
+            }
+
+            cout << "Multiply Matrix Display" << endl;
+            printVector(resultMatrix, theSize);
+        }
+        
+        //Matrix: Major & Minor Diagonal Sum Function
+        void diagonalSum() {
+            double majorDiagonal = 0;
+            double minorDiagonal = 0;
+
+            for (size_t i = 0; i < theSize; ++i) {
+                majorDiagonal += data[i][i];
+                minorDiagonal += data[i][(theSize-1) - i];
+            }
+
+            cout << "Major Diagonal Elements Sum: " << majorDiagonal << endl;
+            cout << "Minor Diagonal Elements Sum: " << minorDiagonal << endl;
         }
 
 
 
 };
+
+//Matrix Selection Function
+bool selectMatrix() {
+    //Local Variables
+    int n = 0; //Store User Input
+    bool selectVal; //User Input Result
+
+    while (n <= 0 || n >= 3) { //Constant Input until a Valid Input
+        cout << "Enter '1' or '2' to Select Matrix 1 or Matrix 2: "; cin >> n; //Input Message and get user input
+        if (n == 1) selectVal = true; //Matrix 1 Selection
+        else if (n == 2) selectVal = false; //Matrix 2 Selection
+        else {
+            cout << "Error 04: Invalid Input" << endl << endl; //Error Message
+        }
+    }
+    return selectVal; //Return the boolean
+}
 
 //Out of Bounds Function
 bool inBounds(const int i, const int n) {
@@ -99,10 +164,35 @@ void matrixResult(Matrix<int> mx1, Matrix<int> mx2) {
     mx2.print();
 
     mx1.add(mx2);
+    mx1.multiply(mx2);
+
+    if (selectMatrix()) {
+        cout << "Matrix 1 Diagonal Sums" << endl;
+        mx1.diagonalSum();
+    } else { 
+        cout << "Matrix 2 Diagonal Sums" << endl; 
+        mx2.diagonalSum();
+    }
+    return;
 }
 
 //Matrix Result Function for Double
 void matrixResult(Matrix<double> mx1, Matrix<double> mx2) {
+    cout << "Matrix 1" << endl;
+    mx1.print();
+    cout << "Matrix 2" << endl;
+    mx2.print();
+
+    mx1.add(mx2);
+    mx1.multiply(mx2);
+
+    if (selectMatrix()) {
+        cout << "Matrix 1 Diagonal Sums" << endl;
+        mx1.diagonalSum();
+    } else { 
+        cout << "Matrix 2 Diagonal Sums" << endl; 
+        mx2.diagonalSum();
+    }
     return;
 }
 
